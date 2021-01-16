450fa8724f21682c5c6cecc7e61e95c62a04b97a
Nive <nivedithakani@gmail.com>
2021-01-16 04:12:33 -0500

    Fix small bug

--- components/Homepage/index.js
+++ components/Homepage/index.js
@@ -7,7 +7,8 @@ import ReviewRequest from "./ReviewRequest"
 import FollowRecs from "./FollowRecs"
 
 function Homepage() {
-  const featurepic = "https://cdn.vox-cdn.com/thumbor/I2bFYczqDpiHcC1cUt_ptziX_t8=/0x0:4896x3264/1820x1213/filters:focal(2057x1241:2839x2023):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/64715574/GettyImages_577660404.0.jpg" avatar="https://bit.ly/dan-abramov"
+  const featurepic = "https://cdn.vox-cdn.com/thumbor/I2bFYczqDpiHcC1cUt_ptziX_t8=/0x0:4896x3264/1820x1213/filters:focal(2057x1241:2839x2023):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/64715574/GettyImages_577660404.0.jpg" 
+  const featureavatar = "https://bit.ly/dan-abramov"
   const featureauthor = "Naruto Uzumaki"
   const featuretitle = "Best article ever"
   const featuretimestamp = "5 hours ago"
@@ -16,7 +17,7 @@ function Homepage() {
     <div>
       <HStack spacing="80px">
           <VStack spacing="20px">
-            <MegaFeature img={featurepic} title={featureauthor} subtitle={featuretitle} timestamp={featuretimestamp}/>
+            <MegaFeature img={featurepic} title={featureauthor} subtitle={featuretitle} avatar={featureavatar} timestamp={featuretimestamp}/>
             <Spacer/>
             <ListSection/>
           </VStack>
