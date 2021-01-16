998ccd0f827b28a3e922a67cbe1493a27c44591c
Jarrod Servilla <45128231+jcserv@users.noreply.github.com>
2021-01-16 01:59:36 -0500

    add footer (#5)
    * add footer, remove unused landmark

---
+++ components/Footer/index.js
@@ -0,0 +1,84 @@
+import { Box, HStack, Icon, Link, Text } from "@chakra-ui/react";
+import React from "react";
+import { DiRuby } from "react-icons/di";
+import { FaDev, FaReact } from "react-icons/fa";
+import styles from "./Footer.module.css";
+
+const links = [
+  {
+    icon: FaReact,
+    label: "GitHub - Frontend",
+    href: "https://github.com/white-van/freeflo-frontend",
+    color: "#61DBFB",
+  },
+  {
+    icon: DiRuby,
+    label: "Github - Backend",
+    href: "https://github.com/white-van/freeflo-backend",
+    color: "#cc0000",
+  },
+  {
+    icon: FaDev,
+    label: "Devpost",
+    href: "https://devpost.com/",
+    color: "#30D5C8",
+  },
+];
+
+const contributors = [
+  {
+    name: "Noredeen Al-Zu'bi",
+    href: "https://github.com/noredeen-alzubi",
+  },
+  {
+    name: "Ritvik Bhardwaj",
+    href: "https://github.com/AipioxTechson",
+  },
+  {
+    name: "Niveditha Kani",
+    href: "https://github.com/NivedithaK",
+  },
+  {
+    name: "Jarrod Servilla",
+    href: "http://jarrodservilla.com",
+  },
+];
+
+const FooterLink = ({ color, icon, href, label }) => (
+  <Link display="inline-block" href={href} aria-label={label} isExternal>
+    <Icon as={icon} fontSize="xl" color="gray.400" _hover={{ color }} />
+  </Link>
+);
+
+const Footer = () => {
+  return (
+    <Box as="footer" className={styles.footer} textAlign="center">
+      <Text mt={4} fontSize="md">
+        Made with{" "}
+        <span aria-label="heart" role="img">
+          &#128156;
+        </span>{" "}
+        by
+        {contributors.map((contributor, index) => (
+          <>
+            {`${index === 3 ? ", & " : ", "}`}
+            <Link key={contributor.href} {...contributor}>
+              {contributor.name}
+            </Link>
+          </>
+        ))}{" "}
+        for{" "}
+        <Link href="https://hackthenorth.com/" isExternal>
+          Hack The North 2020++
+        </Link>
+      </Text>
+      <HStack spacing="12px" justify="center">
+        {links.map((link) => (
+          <FooterLink key={link.href} {...link} />
+        ))}
+      </HStack>
+    </Box>
+  );
+};
+
+export default Footer;
