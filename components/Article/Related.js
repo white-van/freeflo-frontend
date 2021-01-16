c38f2db6ecadda9f3a7108ccbbc725581e537679
Nive <nivedithakani@gmail.com>
2021-01-16 10:57:23 -0500

    Add page for viewing articles

---
+++ components/Article/Related.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link, Box, Flex, Spacer, HStack, Image, Text, Divider } from "@chakra-ui/react";
+
+function Related(props) {
+    return (
+        <div>
+        <HStack w="800px" spacing="20px">
+            <Image
+                htmlWidth="100px"
+                src={props.img}
+            />
+            <div>
+            <Box w="650px">
+                <Flex>
+                    <Link>
+                    <Text fontWeight="bold"> {props.title} </Text>
+                    </Link>
+                    <Spacer />
+                    <Text> {props.timestamp} </Text>
+                </Flex>
+            </Box>
+            <Box w="650px">
+            <Text> {props.subtitle} </Text>
+            </Box>
+            </div>
+            <Spacer />
+        </HStack>
+        <br/>
+        <Divider />
+        </div>
+    );
+}
+
+export default Related;
---
+++ pages/read/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { VStack } from "@chakra-ui/react";
+import Article from "../../components/Article";
+import CommentSection from "../../components/Article/CommentSection";
+import RelatedSection from "../../components/Article/RelatedSection";
+
+function Read() {
+  return (
+  <div>
+    <Article isReadOnly={true} />
+    <CommentSection />
+    <RelatedSection />
+  </div>
+  )}
+
+export default Read;
